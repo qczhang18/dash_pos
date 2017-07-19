872bbec48bec72ce2c0481e0cab4c6cb9078ccb4
Q. Zhang <aznyqjai@gmail.com>
2017-07-19 17:09:34 -0400

    load bootstrap correctly

--- db/schema.rb
+++ db/schema.rb
@@ -24,11 +24,8 @@ ActiveRecord::Schema.define(version: 20170719182414) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
-    t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_items_on_category_id"
   end
 
-  add_foreign_key "items", "categories"
 end
